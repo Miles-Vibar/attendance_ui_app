9576f42c0257f3ff3105dd70ca549e63a1797492
Miles <miles.vibar@mobileoptima.com>
2024-11-15 13:13:43 +0800

    first commit

---
+++ lib/layouts/attandance_layout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AttendanceLayout extends StatelessWidget {
+  const AttendanceLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Appbar'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+
+}
